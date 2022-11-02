52a306bdcd56b2ac1fd83f9580f832bd9199f253
Santiago Quiroga <santiago.quiroga@uptc.edu.co>
2022-11-02 08:50:54 -0500

    data base

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,8 @@
 <?php
 
-$db = mysqli_connect('us-cdbr-east-06.cleardb.net', 'bb3d4bdb', 'b0ddaf5df8de9d', 'heroku_4886c4c015bbbca');
+//$db = mysqli_connect('us-cdbr-east-06.cleardb.net', 'bb3d4bdb', 'b0ddaf5df8de9d', 'heroku_4886c4c015bbbca');
 
+$db = mysqli_connect($_ENV['DB_HOST'],$_ENV['DB_USER'], $_ENV['DB_PASS'],$_ENV['DB_BD']);
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
